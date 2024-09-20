7bb5dccccb40a2564143f4cc19c19ca9716c058b
Kónya Milán <b0konmil@vasvari.org>
2024-09-20 11:27:49 +0200

    Cserelo

--- GenerikusPeldaProjekt/Cserelo.cs
+++ GenerikusPeldaProjekt/Cserelo.cs
@@ -1,7 +1,17 @@
 ﻿namespace GenerikusPeldaProjekt
 {
-    public class Cserelo
+    public class Cserelo<T> where T : class
     {
         // Készítsen generikus osztályt amely tetszőleges két típust felcserél
+        public static void Csere(ref T elso, ref T masodik)
+        {
+            T temp = elso;
+            elso = masodik;
+            masodik = temp;
+        }
+        public static string ToString(T elso, T masodik)
+        {
+            return $"{elso}, {masodik}";
+        }
     }
 }
--- GenerikusPeldaProjekt/Program.cs
+++ GenerikusPeldaProjekt/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using GenerikusPeldaProjekt;
+
+string jegy1 = "5";
+string jegy2 = "4";
+
+Console.WriteLine(Cserelo<string>.ToString(jegy1, jegy2));
+Cserelo<string>.Csere(ref jegy1, ref jegy2);
+Console.WriteLine(Cserelo<string>.ToString(jegy1, jegy2));
+
+Console.WriteLine(jegy1);
+Console.WriteLine(jegy2);
+
+Student lukrecia = new Student()
+{
+    Name = "Lukrécia",
+    Age = 16
+};
+
+Student szerenke = new Student()
+{
+    Name = "Szerénke",
+    Age = 17
+};
+
+Console.WriteLine(Cserelo<Student>.ToString(lukrecia, szerenke));
+Cserelo<Student>.Csere(ref lukrecia, ref szerenke);
